b8ddacb3f3137cbb3cc39096cfb063381080e919
nathanhilbert <nathanhilbert@gmail.com>
2014-09-18 10:21:34 -0400

    further changes

--- archesproject/db/Deployment Scripts/DatabaseCleanup.sql
+++ archesproject/db/Deployment Scripts/DatabaseCleanup.sql
@@ -1,9 +1,9 @@
-SELECT pg_terminate_backend(procpid) from pg_stat_activity where datname='arches';
-SELECT pg_terminate_backend(pid) from pg_stat_activity where datname='arches';
+SELECT pg_terminate_backend(procpid) from pg_stat_activity where datname='arches_staging';
+SELECT pg_terminate_backend(pid) from pg_stat_activity where datname='arches_staging';
 
-DROP DATABASE arches;
+DROP DATABASE arches_staging;
 
-CREATE DATABASE arches
+CREATE DATABASE arches_staging
   WITH ENCODING='UTF8'
        OWNER=postgres
        TEMPLATE=template_postgis_20
--- archesproject/db/Deployment Scripts/Restore.sh
+++ archesproject/db/Deployment Scripts/Restore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # delete all managed schemas
-psql -h localhost -p 5432 -U postgres -d postgres -f "C:\arches-arches-6127ba016596\archesproject\db\Deployment Scripts\DatabaseCleanup.sql"
+psql -h localhost -p 5433 -U postgres -d postgres -f "C:\inetpub\arches_staging\archesproject\db\Deployment Scripts\DatabaseCleanup.sql"
 
 # recreate the database
-psql -h localhost -p 5432 -U postgres -d arches -f "C:\arches-arches-6127ba016596\archesproject\db\Deployment Scripts\Restore.sql"
+psql -h localhost -p 5433 -U postgres -d arches_staging -f "C:\inetpub\arches_staging\archesproject\db\Deployment Scripts\Restore.sql"
