5165590d33e65c1f1e625f73e68b244b0bf7d084
nathanhilbert <nathanhilbert@gmail.com>
2014-09-30 20:46:12 +0000

    forgeting the about virtualenv

--- archesproject/virtualenv/ENV/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\inetpub\arches_staging\archesproject\virtualenv\ENV\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
